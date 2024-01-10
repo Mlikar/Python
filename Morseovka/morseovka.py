99c0f03ccefdd55ff26e88e227095b078aa2520f
Mlikar <115452874+Mlikar@users.noreply.github.com>
2024-01-10 11:52:22 +0000

    Oprava

--- Morseovka/morseovka.py
+++ Morseovka/morseovka.py
@@ -3,7 +3,7 @@ slovo = ""
 result = ""
 
 user_input = input("Zadejte slovo: ")
-slovo = user_input
+slovo = user_input.upper()
 
 with open('abeceda.txt', 'r') as f:
     lines = f.readlines()
@@ -11,18 +11,20 @@ with open('abeceda.txt', 'r') as f:
     pismeno = []
     znaky = []
     
-    for I in lines:
-        as_list = I.strip().split(' ')
-        pismeno.append(as_list[0])
-        znaky.append(as_list[1])
-    
-   # print(pismeno)
-   # print(znaky)
+    for line in lines:
+        as_list = line.strip().split(' ')
 
-    
+        if len(as_list) == 2:
+            pismeno.append(as_list[0])
+            znaky.append(as_list[1])
+        else:
+            pismeno.append(as_list[0])
+            znaky.append(as_list[0])
+
 for char in slovo:
-    result += znaky[pismeno.index(char)] + ' '
+    if char == ' ':
+        result += " -.-.-.- "
+    else:
+        result += znaky[pismeno.index(char)] + ' '
 
-    
-print (result)
-        
+print(result)
